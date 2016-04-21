8ad02910fe7ac4916bdfc9e5b00022b28a883fb5
f1tzgerald <alter.vetal@mail.ru>
2016-04-21 21:22:17 +0300

    add port

--- server.js
+++ server.js
@@ -82,5 +82,5 @@ app.delete('/companies/:id', function (req,res){
 	});
 });
 
-app.listen(3000);
-console.log("Server running on port 3000");
+app.listen(process.env.PORT || 3000);
+console.log("Server running on port ");
